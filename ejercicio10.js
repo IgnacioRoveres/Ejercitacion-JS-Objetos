329ce86fc633f5ab8e3de6ee7b0f80ea18cd164a
ignacioroveres <ignaciovroveres10@gmail.com>
2024-08-06 03:59:46 -0300

    terminado

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,2 +1,13 @@
 
     // Ejercicio 10: Utilizar un bucle for...of para iterar sobre un array de autos e imprimir sus descripciones.
+
+const autos = [
+    {Marca:"Royota", Modelo: "Corolla", Año: 2000},
+    {Marca:"Renault", Modelo: "Clio", Año: 2004},
+    {Marca:"Volkswagen", Modelo: "Golf", Año: 2002},
+];
+
+for (const auto of autos){
+    console.log(`${auto.Marca} ${auto.Modelo} ${auto.Año}`);
+}
+    
--- ejercicio9.js
+++ ejercicio9.js
@@ -1,2 +1,17 @@
 
     // Ejercicio 9: Crear un array de autos y utilizar el método forEach para imprimir la descripción de cada auto.
+
+
+const autos = [
+    {Marca:"Royota", Modelo: "Corolla", Año: 2000},
+    {Marca:"Renault", Modelo: "Clio", Año: 2004},
+    {Marca:"Volkswagen", Modelo: "Golf", Año: 2002},
+];
+
+function imprimirDescripcion (auto){
+    console.log(`${auto.Marca} ${auto.Modelo} ${auto.Año}`);
+}
+
+autos.forEach(imprimirDescripcion);
+
+
